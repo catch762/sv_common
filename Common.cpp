8c1e46337c4b76929e443833a2443f17c6b69ab0
catch762 <hoh.prichesat@gmail.com>
2026-03-13 22:42:49 +0300

    smallfix that doesnt work

--- Common.cpp
+++ Common.cpp
@@ -1,6 +1,7 @@
 #include "Common.h"
 
 std::string getCurrentTimeHMS() {
-    auto now = floor<std::chrono::seconds>(std::chrono::system_clock::now());
+    auto now = std::chrono::current_zone()->to_local(
+        floor<std::chrono::seconds>(std::chrono::system_clock::now()));
     return std::format("{:%H:%M:%S}", now);
 }
