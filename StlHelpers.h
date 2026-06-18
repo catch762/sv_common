79cfaf370293ba8e8baa22e885ea7f76589cfac7
catch762 <hoh.prichesat@gmail.com>
2026-06-18 21:22:06 +0300

    --

--- StlHelpers.h
+++ StlHelpers.h
@@ -1,15 +1,3 @@
 #pragma once
 #include <vector>
 
-template<typename ValueT>
-bool vectorContentEquals(const std::vector<ValueT>& a, const std::vector<ValueT>& b)
-{
-    if (a.size() != b.size()) return false;
-
-    for (int i = 0; i < a.size(); ++i)
-    {
-        if (a[i] != b[i]) return false;
-    }
-
-    return true;
-}
