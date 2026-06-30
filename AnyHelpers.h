d144889f09b961c6875b70296b320addcfa71b38
catch762 <hoh.prichesat@gmail.com>
2026-06-30 20:27:30 +0300

    any helpers

--- AnyHelpers.h
+++ AnyHelpers.h
@@ -1,9 +1,62 @@
 #pragma once
 #include <any>
 #include <typeindex>
+#include "Formatters.h"
+#include <optional>
+#include "TypeMeta/TypeMeta.h"
+#include "Common.h"
+
+using anyOpt = std::optional<std::any>;
 
 //note: if any is empty, it will return std::type_index(typeid(void))
 inline std::type_index typeIndex(const std::any& any)
 {
 	return std::type_index(any.type());
 }
+
+inline std::string anyInfo(const std::any& any)
+{
+	if (!any.has_value())
+	{
+		return "any_empty[]";
+	}
+	else if (auto* myTypeName = TypeNames::getTypeName(typeIndex(any)))
+	{
+		return std::format("any_named[{}]", myTypeName);
+	}
+	else
+	{
+		//prints mangled name
+		return std::format("any_unnamed[{}]", any.type().name());
+	}
+}
+
+template <typename T>
+bool anyHoldsType(const std::any& any)
+{
+	return any.type() == typeid(T);
+}
+
+template <typename T>
+const T* anyGet(const std::any& any)
+{
+	return std::any_cast<T>(&any);
+}
+
+template <typename T>
+T* anyGet(std::any& any)
+{
+	return std::any_cast<T>(&any);
+}
+
+template <typename T>
+std::optional<T> anyGetOpt(const std::any& any)
+{
+	if (auto* val = anyGet<T>(any))
+	{
+		return std::optional<T>(*val);
+	}
+	else return {};
+}
+
+SV_DECL_STD_FORMATTER(std::any, anyInfo(obj));
